b9e85cca4f18cda07f3aad73ba2a61d11546d773
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2022-02-15 14:43:57 +0000

    Test callbacks via Actions

--- go.mod
+++ go.mod
@@ -4,13 +4,15 @@ go 1.17
 
 require (
 	github.com/bmatcuk/doublestar/v4 v4.0.2
-	github.com/riposo/riposo v0.1.1-0.20220210144547-c3359095fe98
+	github.com/riposo/riposo v0.1.1-0.20220215142435-e680adcab55c
 	github.com/santhosh-tekuri/jsonschema/v5 v5.0.0
 	github.com/tidwall/gjson v1.13.0
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 )
 
 require (
+	github.com/alexedwards/argon2id v0.0.0-20211130144151-3585854a6387 // indirect
+	github.com/benbjohnson/clock v1.3.0 // indirect
 	github.com/bsm/nanoid v0.2.0 // indirect
 	github.com/go-chi/chi/v5 v5.0.7 // indirect
 	github.com/google/uuid v1.3.0 // indirect
@@ -19,4 +21,6 @@ require (
 	github.com/tidwall/sjson v1.2.4 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.7.0 // indirect
+	golang.org/x/crypto v0.0.0-20220112180741-5e0467b6c7ce // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
 )
--- internal/callbacks_test.go
+++ internal/callbacks_test.go
@@ -3,6 +3,9 @@ package internal_test
 import (
 	"testing"
 
+	"github.com/riposo/riposo/pkg/api"
+	"github.com/riposo/riposo/pkg/mock"
+	"github.com/riposo/riposo/pkg/riposo"
 	"github.com/riposo/riposo/pkg/schema"
 	"github.com/riposo/schema/internal"
 	"github.com/santhosh-tekuri/jsonschema/v5"
@@ -10,65 +13,103 @@ import (
 
 func TestCallbacks(t *testing.T) {
 	js, err := jsonschema.CompileString("person.json", `{
-		"$id": "mock:///person.json",
-		"$schema": "https://json-schema.org/draft/2020-12/schema",
-		"title": "Person",
-		"type": "object",
-		"required": [ "firstName", "lastName" ],
-		"properties": {
-			"firstName": { "type": "string", "minLength": 3 },
-			"lastName": { "type": "string" }
+		"$id":"mock:///person.json",
+		"$schema":"https://json-schema.org/draft/2020-12/schema",
+		"title":"Person",
+		"type":"object",
+		"required":["firstName","lastName"],
+		"properties":{
+			"firstName":{"type":"string","minLength":3},
+			"lastName":{"type":"string"}
 		}
 	}`)
 	if err != nil {
 		t.Fatalf("expected no error, got %v", err)
 	}
 
-	callbacks := internal.SeedCallbacks(js)
+	act := api.NewActions(api.DefaultModel{}, []api.Callbacks{internal.SeedCallbacks(js)})
+	txn := mock.Txn()
+	defer txn.Abort()
 
-	t.Run("OnCreate", func(t *testing.T) {
-		err := callbacks.OnCreate(nil, "").BeforeCreate(&schema.Resource{
+	t.Run("Create", func(t *testing.T) {
+		err = act.Create(txn, "/buckets/foo/people/*", &schema.Resource{
 			Data: &schema.Object{Extra: []byte(`{}`)},
 		})
 		if exp := `data in body: '' does not validate with mock:///person.json#/required: missing properties: 'firstName', 'lastName'`; exp != err.Error() {
 			t.Fatalf("expected %v, got %v", exp, err)
 		}
 
-		err = callbacks.OnCreate(nil, "").BeforeCreate(&schema.Resource{
-			Data: &schema.Object{Extra: []byte(`{ "firstName": "J", "lastName": "Doe" }`)},
+		err = act.Create(txn, "/buckets/foo/people/*", &schema.Resource{
+			Data: &schema.Object{Extra: []byte(`{"firstName":"J","lastName":"Doe"}`)},
 		})
 		if exp := `data in body: '/firstName' does not validate with mock:///person.json#/properties/firstName/minLength: length must be >= 3, but got 1`; exp != err.Error() {
 			t.Fatalf("expected %v, got %v", exp, err)
 		}
 
-		err = callbacks.OnCreate(nil, "").BeforeCreate(&schema.Resource{
-			Data: &schema.Object{Extra: []byte(`{ "firstName": "Jane", "lastName": "Doe" }`)},
+		err = act.Create(txn, "/buckets/foo/people/*", &schema.Resource{
+			Data: &schema.Object{Extra: []byte(`{"firstName":"Jane","lastName":"Doe"}`)},
 		})
 		if err != nil {
 			t.Fatalf("expected no error, got %v", err)
 		}
 	})
 
-	t.Run("OnUpdate", func(t *testing.T) {
-		err := callbacks.OnUpdate(nil, "").BeforeUpdate(nil, &schema.Resource{
-			Data: &schema.Object{Extra: []byte(`{ "firstName": "J", "lastName": "Doe" }`)},
+	t.Run("Update", func(t *testing.T) {
+		res := &schema.Resource{
+			Data: &schema.Object{Extra: []byte(`{"firstName":"Jane","lastName":"Doe"}`)},
+		}
+		if err := act.Create(txn, "/buckets/foo/people/*", res); err != nil {
+			t.Fatalf("expected no error, got %v", err)
+		}
+		path := riposo.Path("/buckets/foo/people/" + res.Data.ID)
+
+		handle, err := txn.Store.GetForUpdate(path)
+		if err != nil {
+			t.Fatalf("expected no error, got %v", err)
+		}
+
+		res, err = act.Update(txn, path, handle, &schema.Resource{
+			Data: &schema.Object{Extra: []byte(`{"firstName":"Alice","lastName":"Glass"}`)},
+		})
+		if err != nil {
+			t.Fatalf("expected no error, got %v", err)
+		} else if exp, got := `{"id":"ITR.ID","last_modified":1515151515679,"firstName":"Alice","lastName":"Glass"}`, res.Data.String(); exp != got {
+			t.Fatalf("expected %v, got %v", exp, got)
+		}
+
+		_, err = act.Update(txn, path, handle, &schema.Resource{
+			Data: &schema.Object{Extra: []byte(`{"firstName":"J","lastName":"Doe"}`)},
 		})
 		if exp := `data in body: '/firstName' does not validate with mock:///person.json#/properties/firstName/minLength: length must be >= 3, but got 1`; exp != err.Error() {
 			t.Fatalf("expected %v, got %v", exp, err)
 		}
 	})
 
-	t.Run("OnPatch", func(t *testing.T) {
-		exst := &schema.Object{Extra: []byte(`{ "firstName": "Jane", "lastName": "Doe" }`)}
-		err := callbacks.OnPatch(nil, "").BeforePatch(exst, &schema.Resource{
-			Data: &schema.Object{Extra: []byte(`{ "firstName": "Alice" }`)},
-		})
+	t.Run("Patch", func(t *testing.T) {
+		res := &schema.Resource{
+			Data: &schema.Object{Extra: []byte(`{"firstName":"Jane","lastName":"Doe"}`)},
+		}
+		if err := act.Create(txn, "/buckets/foo/people/*", res); err != nil {
+			t.Fatalf("expected no error, got %v", err)
+		}
+		path := riposo.Path("/buckets/foo/people/" + res.Data.ID)
+
+		handle, err := txn.Store.GetForUpdate(path)
 		if err != nil {
 			t.Fatalf("expected no error, got %v", err)
 		}
 
-		err = callbacks.OnPatch(nil, "").BeforePatch(exst, &schema.Resource{
-			Data: &schema.Object{Extra: []byte(`{ "firstName": "J" }`)},
+		res, err = act.Patch(txn, path, handle, &schema.Resource{
+			Data: &schema.Object{Extra: []byte(`{"firstName":"Alice"}`)},
+		})
+		if err != nil {
+			t.Fatalf("expected no error, got %v", err)
+		} else if exp, got := `{"id":"MXR.ID","last_modified":1515151515681,"firstName":"Alice","lastName":"Doe"}`, res.Data.String(); exp != got {
+			t.Fatalf("expected %v, got %v", exp, got)
+		}
+
+		_, err = act.Patch(txn, path, handle, &schema.Resource{
+			Data: &schema.Object{Extra: []byte(`{"firstName":"J"}`)},
 		})
 		if exp := `data in body: '/firstName' does not validate with mock:///person.json#/properties/firstName/minLength: length must be >= 3, but got 1`; exp != err.Error() {
 			t.Fatalf("expected %v, got %v", exp, err)
